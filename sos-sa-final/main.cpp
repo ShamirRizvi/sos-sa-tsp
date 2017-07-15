f5d302e36c2daa1fb029fac4c4da1de671ba0f80
Sebastian Coronado <sebastian@amigocloud.com>
2017-07-15 18:49:08 -0500

    Initial Commit

---
+++ sos-sa-final/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  sos-sa-final
+//
+//  Created by Sebastian Coronado on 15/07/17.
+//  Copyright © 2017 Sebastian Coronado. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
